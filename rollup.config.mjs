eda181f0744528c5ad3f80c42cfb1e360aa76a8a
Charley <rlawlgh3894@naver.com>
2024-10-06 17:43:18 +0900

    chore: peer react version 설정

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import babel from '@rollup/plugin-babel';
@@ -21,7 +21,7 @@ export default {
     },
   ],
   plugins: [
-    peerDepsExternal(),
+    PeerDepsExternalPlugin(),
     resolve(),
     commonjs(),
     typescript({ tsconfig: './tsconfig.json' }),
